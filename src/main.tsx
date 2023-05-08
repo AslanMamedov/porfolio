1a6deaf0069d32c10958129e8191413ffe1ef318
Aslan Mammadov <aslan94mamedov49@gmail.com>
2023-05-08 20:21:22 +0400

    hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, router, theme } from 'config/index.ts';
 import './18n.ts';
 import { Loader } from 'components/Loader.tsx';
 
-interface ITheme {
-	[key: string]: any;
-	mode: 'light' | 'dark';
-}
-const themes: ITheme = {
+const themes = {
 	...theme,
 	mode: 'dark',
 };
